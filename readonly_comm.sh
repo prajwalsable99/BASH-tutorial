0f5c89c2ea44e6cfe4d89a741df102e9bb8d69fe
prajwal <prajsa99@gmail.com>
2022-10-06 18:31:49 +0530

    four commit

---
+++ readonly_comm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+var=31
+
+readonly var 
+
+var=50
--- upload2.sh
+++ upload2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m "third commit"
+git commit -m "four commit"
 git branch -M main
 git push -u origin main
